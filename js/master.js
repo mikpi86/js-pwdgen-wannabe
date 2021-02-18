ddc7ae05daf51e6ca66e73105ffa0c4a3b6382c3
mikpi86 <piras.michela86@gmail.com>
2021-02-18 15:41:44 +0100

    inizio

---
+++ js/master.js
@@ -0,0 +1,14 @@
+//comincio inizializzando la variabile del nome
+var nome;
+//assegno alla variabile nome il valore della risposta al prompt
+nome = prompt('Qual è il tuo nome?');
+//stampo la risposta nella console
+console.log(nome);
+
+//inizializzo la variabile del cognome
+var cognome;
+//assegno alla variabile del cognome il valore della risposta al prompt
+cognome = prompt('E il tuo cognome?');
+//stampo la risposta nella console
+console.log(cognome);
+
